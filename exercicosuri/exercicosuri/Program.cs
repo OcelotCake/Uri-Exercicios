84e6a89f5c847491992647b9568844e3ceae963e
CHARLESKUNDEVITALE <CHARLESKUNDEVITALE7@gmail.com>
2025-04-03 19:26:17 -0300

    test of  1005

---
+++ exercicosuri/exercicosuri/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+# include<stdio.h>
+
+        int main()
+        {
+            float A, B, MEDIA;
+
+            scanf("%f", &A);
+            scanf("%f", &B);
+
+            MEDIA = (A * 3.5 + B * 7.5) / 11;
+
+            printf("MEDIA = %.5f\n", MEDIA);
+
+            return 0;
+        }
+
+    }
